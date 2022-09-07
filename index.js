23b50094c43432e9ccffc09b9a10b821f7000d99
bissan <bissansaad2@gmail.com>
2022-09-07 19:37:28 +0300

    finally done the project

---
+++ index.js
@@ -0,0 +1,15 @@
+document.getElementById("allert").addEventListener("mouseover", function () {
+    Swal.fire({
+      text: "ابذل ما بوسعك",
+      title:" أهلا بك ، انتبه للشرح",
+      showConfirmButton: true,
+      confirmButtonColor: "##001858",
+      color: "#001858",
+      imageUrl:
+        "https://c.tenor.com/qc89FmVP6GwAAAAM/studying-busy.gif",
+      imageWidth: 400,
+      imageHeight: 200,
+    });
+  });
+
+ 
